97f929e032e66cc1c3cd7ffadb711cd3c43d4196
Irene <irene@usemanifest.com>
2024-01-11 11:21:16 -0600

    chore(all): make scope required

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     // Place your rules here
-    'scope-enum': [2, 'always', ['a', 'b']], // error if scope is given but not in provided list
+    "scope-enum": [2, "always", ["child-1", "child-2", "all"]], // error if scope is given but not in provided list,
+    'scope-empty': [2, 'never']
   },
 };
