a3683c45c1cd3a90f2e6a65d471dcbe85e558996
Irene <irene@usemanifest.com>
2024-01-11 11:20:03 -0600

    feat: try out scope enum

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-
-  scope: {
-    required: true, // 'scope-empty': [2, 'never']
-    allowed: ["a", "b"], // 'scope-enum': [2, 'always', ['a', 'b']]; specify [0] for allowed: ["*"]
-    validate: false, // 'scope-enum': [0], 'scope-empty': [0]
-    multiple: false,
+  rules: {
+    // Place your rules here
+    'scope-enum': [2, 'always', ['a', 'b']], // error if scope is given but not in provided list
   },
 };
