1a0231b89a7479ef3e3a99dd407a9cd0111b9d91
Irene <irene@usemanifest.com>
2024-01-11 11:56:02 -0600

    chore(all): add semantic release packages

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,7 @@ module.exports = {
   rules: {
     // Place your rules here
     "scope-enum": [2, "always", ["child-1", "child-2", "all"]], // error if scope is given but not in provided list,
-    'scope-empty': [2, 'never']
+    'scope-empty': [2, 'never'],
+    "type-enum":[2, "always", ["feat", "fix", "styles", "refactor", "pref", "test", "chore"]]
   },
 };
