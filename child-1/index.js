50e4e344fc4bf0058ad3ce9be11ce0f2a3721706
Irene <irene@usemanifest.com>
2024-01-11 14:48:13 -0600

    fix(child-1): add function

--- child-1/index.js
+++ child-1/index.js
@@ -1,8 +1,8 @@
-console.log('from child 1')
-const add = (a, b)=>{
-    return a+b
-}
+console.log("from child 1");
+const add = (a, b) => {
+  return a + b;
+};
 
-const multiply = (a, b)=>{
-    return a * b;
-}
+const multiply = (a, b) => {
+  return a * b;
+};
