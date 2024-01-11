b79eaa47a6d55ebb3789b303403c8673e979824e
Irene <irene@usemanifest.com>
2024-01-11 11:16:40 -0600

    feat(all): try out scope enum

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,10 @@
-module.exports = { extends: ['@commitlint/config-conventional'] ,
-'scope-enum': [2, 'always', ['frontend', 'backend']], // error if scope is given but not in provided listm
-"scope": {
-    "required": true,               // 'scope-empty': [2, 'never']
-    "allowed": ["a", "b"],          // 'scope-enum': [2, 'always', ['a', 'b']]; specify [0] for allowed: ["*"]
-    "validate": false,              // 'scope-enum': [0], 'scope-empty': [0]
-    "multiple": false        
-}
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+
+  scope: {
+    required: true, // 'scope-empty': [2, 'never']
+    allowed: ["a", "b"], // 'scope-enum': [2, 'always', ['a', 'b']]; specify [0] for allowed: ["*"]
+    validate: false, // 'scope-enum': [0], 'scope-empty': [0]
+    multiple: false,
+  },
 };
