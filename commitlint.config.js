13cf079b09dd76117281ddc18a9bd0dd290d83a0
Irene <irene@usemanifest.com>
2024-01-11 14:36:29 -0600

    feat(all): add release ci

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,6 +4,6 @@ module.exports = {
     // Place your rules here
     "scope-enum": [2, "always", ["child-1", "child-2", "all"]], // error if scope is given but not in provided list,
     'scope-empty': [2, 'never'],
-    "type-enum":[2, "always", ["feat", "fix", "styles", "refactor", "pref", "test", "chore"]]
+    "type-enum":[2, "always", ["feat", "feature", "fix", "styles", "refactor", "pref", "test", "chore"]]
   },
 };
