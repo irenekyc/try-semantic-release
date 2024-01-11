727ad3ca77299986c80247819b1af0f46f5b63b0
Irene <irene@usemanifest.com>
2024-01-11 14:49:59 -0600

    feat(child-2): add divide function

--- child-2/index.js
+++ child-2/index.js
@@ -1,5 +1,9 @@
-console.log('from child 2')
+console.log("from child 2");
 
-const minus = (a, b)=>{
-    return a-b
-}
+const minus = (a, b) => {
+  return a - b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
